4c99a621fa103f327902b35112d0c229cdd197a5
alonedokdps <63144996+alonedokdps@users.noreply.github.com>
2022-01-24 14:47:59 +0700

    update 24/1

--- tmovie/src/api/axiosClient.js
+++ tmovie/src/api/axiosClient.js
@@ -1,24 +1,30 @@
 import axios from "axios";
 import queryString from "query-string";
+
 import apiConfig from "./apiConfig";
+
 const axiosClient = axios.create({
   baseURL: apiConfig.baseUrl,
   headers: {
-    "content-type": "application/json",
+    "Content-Type": "application/json",
   },
   paramsSerializer: (params) =>
     queryString.stringify({...params, api_key: apiConfig.apiKey}),
 });
+
 axiosClient.interceptors.request.use(async (config) => config);
-axiosClient.interceptors.request.use(
+
+axiosClient.interceptors.response.use(
   (response) => {
     if (response && response.data) {
       return response.data;
     }
+
     return response;
   },
   (error) => {
     throw error;
   }
 );
+
 export default axiosClient;
--- tmovie/src/api/tmdbApi.js
+++ tmovie/src/api/tmdbApi.js
@@ -1,20 +1,24 @@
 import axiosClient from "./axiosClient";
+
 export const category = {
   movie: "movie",
   tv: "tv",
 };
+
 export const movieType = {
   upcoming: "upcoming",
   popular: "popular",
   top_rated: "top_rated",
 };
+
 export const tvType = {
   popular: "popular",
   top_rated: "top_rated",
   on_the_air: "on_the_air",
 };
+
 const tmdbApi = {
-  getMovieList: (type, params) => {
+  getMoviesList: (type, params) => {
     const url = "movie/" + movieType[type];
     return axiosClient.get(url, params);
   },
@@ -23,14 +27,13 @@ const tmdbApi = {
     return axiosClient.get(url, params);
   },
   getVideos: (cate, id) => {
-    const url = category[cate] + "/" + id + "/video";
+    const url = category[cate] + "/" + id + "/videos";
     return axiosClient.get(url, {params: {}});
   },
   search: (cate, params) => {
     const url = "search/" + category[cate];
     return axiosClient.get(url, params);
   },
-
   detail: (cate, id, params) => {
     const url = category[cate] + "/" + id;
     return axiosClient.get(url, params);
@@ -44,4 +47,5 @@ const tmdbApi = {
     return axiosClient.get(url, {params: {}});
   },
 };
+
 export default tmdbApi;
